efddc5df07c8cf9ad3a3afc55c9daa8a46bdd526
Gilles FELIX <felix@novius.com>
2014-06-19 15:07:02 +0200

    tpvar_data can not be initialise, cast to array

--- views/bootstrap/index.view.php
+++ views/bootstrap/index.view.php
@@ -12,16 +12,13 @@
 
 $config = \Config::get('noviusos_template_bootstrap::template');
 
-foreach ($page->template_variation->tpvar_data as $key => $value) {
-    if($key == 'principal-background_style' && $value == '')
-    {
+// cast to array because data can not be initialise
+foreach ((array) $page->template_variation->tpvar_data as $key => $value) {
+    if ($key == 'principal-background_style' && $value == '') {
         $config['principal']['background_style']='';
-    }
-    else if($key == 'principal-background_fixed_display' && $value == '')
-    {
+    } elseif ($key == 'principal-background_fixed_display' && $value == '') {
         $config['principal']['background_fixed_display']='';
-    }
-    elseif ($key == '_sidebar-display') {
+    } elseif ($key == '_sidebar-display') {
         switch($value) {
             case 'left':
                 $config['left']['display'] = true;
@@ -43,15 +40,13 @@ foreach ($page->template_variation->tpvar_data as $key => $value) {
                 $config['right']['display'] = false;
                 break;
         }
-    }
-    else if ($value != '' && $value != null && strstr($key, '-display') == false) {
+    } elseif ($value != '' && $value != null && strstr($key, '-display') == false) {
         if ($key == '_input_hidden_left') {
             $tab_temp = explode('||', $value);
             $tab_gauche_old =  arr::get($config, 'left.blocks');
 
             foreach ($tab_temp as $key => $value) {
-                if($value != "")
-                {
+                if ($value != '') {
                     $temp_key = str_replace('-', '.', $value);
                     $temp_key = str_replace('.display', '', $temp_key);
                     $tab_temp_value = explode('-', $value);
@@ -68,8 +63,7 @@ foreach ($page->template_variation->tpvar_data as $key => $value) {
 
 
             foreach ($tab_temp as $key => $value) {
-                if($value != "")
-                {
+                if ($value != '') {
                     $temp_key = str_replace('-', '.', $value);
                     $temp_key = str_replace('.display', '', $temp_key);
                     $tab_temp_value = explode('-', $value);
@@ -97,18 +91,17 @@ if (!empty($page->template_variation->medias->logo)) {
     $config['header']['logo_url'] = $page->template_variation->medias->logo->url();
 }
 
-if(!isset($config['wysiwyg_layout']) || $config['wysiwyg_layout'] == "")
-{
-    $temp = "";
+if (!isset($config['wysiwyg_layout']) || $config['wysiwyg_layout'] == '') {
+    $temp = '';
 
     for ($i = 0; $i < 12; $i++) {
-        for ( $j = 0; $j < 12; $j++) {
+        for ($j = 0; $j < 12; $j++) {
             $temp .= "1 ";
         }
-        $temp = substr( $temp, 0 , strlen($temp)-1);
+        $temp = substr($temp, 0, strlen($temp) - 1);
         $temp .= "|";
     }
-    $temp = substr( $temp, 0 , strlen($temp)-1);
+    $temp = substr($temp, 0, strlen($temp) - 1);
     $config['wysiwyg_layout']= $temp;
 }
 
@@ -120,27 +113,27 @@ if(!isset($config['wysiwyg_layout']) || $config['wysiwyg_layout'] == "")
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-<?php
-$tab_skin = array_diff(scandir('static/apps/noviusos_template_bootstrap/vendor/'.$template.'/css/skin/'), array('..', '.'));
-
-$str_theme_name = $template;
-$str_skin_name = $config['principal']['skin'];
-foreach ($tab_skin as $key => $value) {
-    $chaine = explode('.css', $value);
-
-    if ($chaine[0] == $str_skin_name) {
-        ?>
-        <link title="<?= $chaine[0] ?>" rel="stylesheet" type="text/css"
-              href="static/apps/noviusos_template_bootstrap/vendor/<?= $str_theme_name ?>/css/skin/<?= $value ?>">
-        <?php
-    } else {
-        ?>
-        <link title="<?= $chaine[0] ?>" rel="alternate stylesheet" type="text/css"
-              href="static/apps/noviusos_template_bootstrap/vendor/<?= $str_theme_name ?>/css/skin/<?= $value ?>">
     <?php
+    $tab_skin = array_diff(scandir('static/apps/noviusos_template_bootstrap/vendor/'.$template.'/css/skin/'), array('..', '.'));
+
+    $str_theme_name = $template;
+    $str_skin_name = $config['principal']['skin'];
+    foreach ($tab_skin as $key => $value) {
+        $chaine = explode('.css', $value);
+
+        if ($chaine[0] == $str_skin_name) {
+            ?>
+            <link title="<?= $chaine[0] ?>" rel="stylesheet" type="text/css"
+                  href="static/apps/noviusos_template_bootstrap/vendor/<?= $str_theme_name ?>/css/skin/<?= $value ?>">
+        <?php
+        } else {
+            ?>
+            <link title="<?= $chaine[0] ?>" rel="alternate stylesheet" type="text/css"
+                  href="static/apps/noviusos_template_bootstrap/vendor/<?= $str_theme_name ?>/css/skin/<?= $value ?>">
+        <?php
+        }
     }
-}
-?>
+    ?>
     <link rel="stylesheet"
           href="static/apps/noviusos_template_bootstrap/vendor/<?= $str_theme_name ?>/css/social-buttons-3.css">
 
@@ -184,7 +177,7 @@ foreach ($tab_skin as $key => $value) {
 
 
 <body id="principal"
-    class="customisable title_page"
+      class="customisable title_page"
     <?= $config['principal']['background_image'] != '' ?
         'style="background-image: url(\''.$config['principal']['background_image'].'\'); '
         .$config['principal']['background_style']
@@ -194,38 +187,38 @@ foreach ($tab_skin as $key => $value) {
 <div id="content" class="col-md-12" style="padding: 0">
 
     <div id="content_inner">
-<?php
-$view = \View::forge('noviusos_template_bootstrap::bootstrap/header', array(
-    'template' => $template,
-    'page' => $page,
-    'title' => $title,
-    'wysiwyg' => $wysiwyg,
-    'dom_id' => $dom_id,
-    'current_context' => $page->get_context(),
-), false);
-$view->config = $config;
-echo $view;
+        <?php
+        $view = \View::forge('noviusos_template_bootstrap::bootstrap/header', array(
+            'template' => $template,
+            'page' => $page,
+            'title' => $title,
+            'wysiwyg' => $wysiwyg,
+            'dom_id' => $dom_id,
+            'current_context' => $page->get_context(),
+        ), false);
+        $view->config = $config;
+        echo $view;
 
-$view = View::forge('noviusos_template_bootstrap::bootstrap/content', array(
-    'page' => $page,
-    'template' => $template,
-    'title' => $title,
-    'wysiwyg' => $wysiwyg,
-    'dom_id' => $dom_id,
-), false);
-$view->config = $config;
-echo $view;
+        $view = View::forge('noviusos_template_bootstrap::bootstrap/content', array(
+            'page' => $page,
+            'template' => $template,
+            'title' => $title,
+            'wysiwyg' => $wysiwyg,
+            'dom_id' => $dom_id,
+        ), false);
+        $view->config = $config;
+        echo $view;
 
-$view = View::forge('noviusos_template_bootstrap::bootstrap/footer', array(
-    'page' => $page,
-    'template' => $template,
-    'title' => $title,
-    'wysiwyg' => $wysiwyg,
-    'dom_id' => $dom_id,
-), false);
-$view->config = $config;
-echo $view;
-?>
+        $view = View::forge('noviusos_template_bootstrap::bootstrap/footer', array(
+            'page' => $page,
+            'template' => $template,
+            'title' => $title,
+            'wysiwyg' => $wysiwyg,
+            'dom_id' => $dom_id,
+        ), false);
+        $view->config = $config;
+        echo $view;
+        ?>
     </div>
 </div>
 </div>
